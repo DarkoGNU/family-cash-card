450a159e504f54654df586fb4dfd49adcc04836b
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2024-02-17 15:02:25 +0000

    Fix code (hopefully)

--- src/main/java/example/cashcard/SecurityConfig.java
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -1,3 +1,5 @@
+package example.cashcard;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -16,10 +18,10 @@ class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                // Permit access to springdoc-openapi-ui endpoints without authentication
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                        .requestMatchers("/cashcards/**").hasRole("CARD-OWNER"))
+                        .requestMatchers("/cashcards/**")
+                        .hasRole("CARD-OWNER"))
                 .httpBasic(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable());
         return http.build();
